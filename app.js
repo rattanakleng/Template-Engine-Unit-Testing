610113089b8aeb434530ea766ca94f1c1b107dae
Rattanak Leng <rleng@live.com>
2020-12-13 21:46:15 -0800

    update app.js--create questions lists, employee list and html render function

--- app.js
+++ app.js
@@ -10,7 +10,196 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeeList = [];
 
+// Ask for manager info
+const askUserForManagerInfo = () => {
+
+    return inquirer.prompt(
+        [
+            {
+                message: "What is the manager's name?",
+                name: "name",
+                type: "input",
+            },
+            {
+                message: "What is the manager's id",
+                name: "id",
+                type: "input"
+            },
+            {
+                message: "What is the manager's email?",
+                name: "email",
+                type: "input"
+            },
+            {
+                message: "What is the manager's office number?",
+                name: "officeNumber",
+                type: "input"
+            }
+        ]).then((managerData) => {
+            const newManager = new Manager(managerData.name, managerData.id, managerData.email, managerData.officeNumber)
+
+
+            // console.log(newManager);
+
+            employeeList.push(newManager)
+
+            console.log(employeeList);
+
+            askEmployeeRole();
+        });
+}
+
+//Ask user for next employee type
+const askEmployeeRole = () => {
+    inquirer.prompt([{
+        message: "Do you want to add employee information?",
+        name: "role",
+        choices: ["engineer", "intern", "no more employee"],
+        type: "list"
+    }]).then((nextEmployee) => {
+
+        console.log(nextEmployee);
+        if (nextEmployee.role === "engineer") {
+
+            askEngineerInfo();
+        } else if (nextEmployee.role === "intern") {
+
+            askInternInfo();
+        } else {
+            createHtmlFile();
+        }
+    })
+}
+
+
+
+
+// Ask user for next employee type
+// askEmployeeType = () => {
+//     inquirer.prompt([{
+//         message: "Do you want to add employee information?",
+//         name: "employeeChoice",
+//         type: "confirm",
+//     }]).then((newEmployeeChoice) => {
+
+//         console.log(newEmployeeChoice);
+//         if (newEmployeeChoice) {
+
+//             inquirer.prompt({
+//                 message: "Do you want to add Engineer?",
+//                 name: "true or false",
+//                 type: "confirm"
+//             }).then((engineer) => {
+//                 if (engineer === true) {
+//                     askEngineerInfo();
+//                 } else {
+//                     askInternInfo();
+//                 }
+//             })
+
+//         } else {
+//             console.log("user does not want to add employee!")
+//             return;
+//         }
+//     })
+// }
+
+// Ask for enginner info (name, id, email, github)
+const askEngineerInfo = () => {
+    // console.log("User select engineer")
+    inquirer.prompt(
+        [
+            {
+                message: "What is the engineer name?",
+                name: "name",
+                type: "input",
+            },
+            {
+                message: "What is the engineer's id",
+                name: "id",
+                type: "input"
+            },
+            {
+                message: "What is the's email?",
+                name: "email",
+                type: "input"
+            },
+            {
+                message: "What is engineer's GitHub user name?",
+                name: "github",
+                type: "input"
+            }
+        ]).then((engineerData) => {
+            const newEngineer = new Engineer(engineerData.name, engineerData.id, engineerData.email, engineerData.github)
+
+            employeeList.push(newEngineer)
+            console.log(employeeList);
+
+            askEmployeeRole();
+        });
+}
+
+const askInternInfo = () => {
+    inquirer.prompt([{
+        message: 'What is the interns name?',
+        name: 'name',
+        type: 'input',
+    },
+    {
+        message: 'What is the interns id?',
+        name: 'id',
+        type: 'input'
+    },
+    {
+        message: 'What is the interns email?',
+        name: 'email',
+        type: 'input'
+    },
+    {
+        message: 'What school does the intern currently attend?',
+        name: 'school',
+        type: 'input'
+    }]).then((internData) => {
+        const newIntern = new Intern(internData.name, internData.id, internData.email, internData.school)
+
+        employeeList.push(newIntern)
+        console.log(employeeList);
+
+        askEmployeeRole();
+    });
+
+}
+// Ask user for engineer info
+// function askUserForEngineerInfo() {
+
+
+// };
+
+// Ask user for intern
+// function askUserForInternInfo() {
+
+
+// };
+
+const createHtmlFile = () => {
+    // const htmlContent = render(employee)
+
+
+    const htmlContent = render(employeeList);
+    // fs mod to write html
+    fs.writeFile(__dirname + '/main.html', htmlContent, (err) => {
+        // check for error
+        err ?
+            // if failed console log 
+            console.log('Failed to write file') :
+            // if success console log
+            console.log('File has been writen');
+    })
+};
+
+askUserForManagerInfo();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,4 +1,6 @@
+const inquirer = require("inquirer");
 const Employee = require("../lib/Employee");
+const Engineer = require("../lib/Engineer");
 
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
@@ -46,3 +48,9 @@ test("getRole() should return \"Employee\"", () => {
   const e = new Employee("Alice", 1, "test@test.com");
   expect(e.getRole()).toBe(testValue);
 });
+
+
+
+
+
+
