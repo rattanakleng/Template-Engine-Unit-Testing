3223f1c155821cb03b1a348e9b2d60efbf1422e0
Rattanak Leng <rleng@live.com>
2020-12-14 21:49:54 -0800

    update app.js -- fix question and main.html rendering

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const employeeList = [];
 // Ask for manager info
 const askUserForManagerInfo = () => {
 
-    return inquirer.prompt(
+    inquirer.prompt(
         [
             {
                 message: "What is the manager's name?",
@@ -72,39 +72,6 @@ const askEmployeeRole = () => {
         }
     })
 }
-
-
-
-
-// Ask user for next employee type
-// askEmployeeType = () => {
-//     inquirer.prompt([{
-//         message: "Do you want to add employee information?",
-//         name: "employeeChoice",
-//         type: "confirm",
-//     }]).then((newEmployeeChoice) => {
-
-//         console.log(newEmployeeChoice);
-//         if (newEmployeeChoice) {
-
-//             inquirer.prompt({
-//                 message: "Do you want to add Engineer?",
-//                 name: "true or false",
-//                 type: "confirm"
-//             }).then((engineer) => {
-//                 if (engineer === true) {
-//                     askEngineerInfo();
-//                 } else {
-//                     askInternInfo();
-//                 }
-//             })
-
-//         } else {
-//             console.log("user does not want to add employee!")
-//             return;
-//         }
-//     })
-// }
 
 // Ask for enginner info (name, id, email, github)
 const askEngineerInfo = () => {
@@ -171,17 +138,6 @@ const askInternInfo = () => {
     });
 
 }
-// Ask user for engineer info
-// function askUserForEngineerInfo() {
-
-
-// };
-
-// Ask user for intern
-// function askUserForInternInfo() {
-
-
-// };
 
 const createHtmlFile = () => {
     // const htmlContent = render(employee)
@@ -200,25 +156,3 @@ const createHtmlFile = () => {
 };
 
 askUserForManagerInfo();
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
